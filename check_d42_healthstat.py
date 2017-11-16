376f6bc739eee5ed538b606c5bacfb83ad2928e8
Arjen Annes <arjen.annes@tomtom.com>
2017-11-16 12:29:57 +0100

    Added author and description

--- check_d42_healthstat.py
+++ check_d42_healthstat.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# -- Author:       github:blafkachel
+# -- Description:  Device42 Nagios healthcheck + performance metrics
+
 
 import sys
 import codecs
